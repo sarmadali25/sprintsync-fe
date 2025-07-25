08c5c346a645ea42afba3beb11d7687cc49b3c6a
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-25 14:16:01 +0500

    fix: main layout height

--- src/layout/main-layout/MainLayout.tsx
+++ src/layout/main-layout/MainLayout.tsx
@@ -1,12 +1,13 @@
 import MobileNavbar from "../../components/navbar/MobileNavbar";
 import Navbar from "../../components/navbar/Navbar";
 import useMediaQuery from "../../hooks/useMediaQuery";
+import { cn } from "../../utils";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const isDesktop = useMediaQuery("(min-width: 1001px)");
 
   return (
-    <div className="w-screen min-h-screen bg-gray-100">
+    <div className="w-screen min-h-screen bg-gray-100 ">
       {!isDesktop &&
       <div className="sticky top-0 z-10 h-[60px]">
        <MobileNavbar />
@@ -14,7 +15,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       }
       {isDesktop && <Navbar />}
       <main className="pt-0 lg:pt-20 bg-white">
-        <div className="min-h-screen">
+        <div className={cn( isDesktop ? "h-[calc(100vh-90px)]" : "h-[calc(100vh-60px)]")}>
           {children}
         </div>
       </main>
