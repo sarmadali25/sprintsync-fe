fbb3fe642d7eb605f0528bdee8d77c9adc79bcc3
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-25 15:59:10 +0500

    feat: update the button colors

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,8 +5,8 @@ const Button = ({ children, onClick, className, variant="primary" }: any) => {
     <button
       className={cn(
         "group border-2 rounded-md px-4 py-1 hover:scale-105 cursor-pointer transition-all duration-300",
-        variant === "primary" && "border-primary  hover:bg-primary text-primary hover:text-white",
-        variant === "secondary" && "border-primary bg-primary hover:bg-white text-white hover:text-primary",
+        variant === "primary" && "border-blue-600 hover:bg-gradient-to-r hover:from-blue-600 hover:to-purple-600 text-blue-600 hover:text-white",
+        variant === "secondary" && "bg-gradient-to-r from-blue-600 to-purple-600 hover:bg-white hover:from-transparent hover:to-transparent text-white hover:text-blue-600 border-blue-600",
         className
       )}
       onClick={onClick}
