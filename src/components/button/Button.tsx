7f127774565ba17b2da1f338ac3e6b0e73f4a2dc
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-25 14:39:01 +0500

    fix: button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,40 +1,17 @@
 import { cn } from "../../utils";
-import { Video } from "lucide-react";
 
-const Button = ({
-  children,
-  onClick,
-  className,
-  small = false,
-}: ButtonProps) => {
+const Button = ({ children, onClick, className, variant="primary" }: any) => {
   return (
     <button
       className={cn(
-        "px-4 py-2 rounded-lg text-sm hover:scale-110 transition-all duration-300 hover:shadow-2xl relative group cursor-pointer border-3 border-primary hover:border-0",
-        className,
-        " hover:bg-gradient-to-r hover:from-tertiary hover:to-tertiary-light"
+        "group border-2 rounded-md px-4 py-1 hover:scale-105 cursor-pointer transition-all duration-300",
+        variant === "primary" && "border-primary  hover:bg-primary text-primary hover:text-white",
+        variant === "secondary" && "border-primary bg-primary hover:bg-white text-white hover:text-primary",
+        className
       )}
-      onClick={
-        onClick
-          ? onClick
-          : () => {
-              window.open(
-                "https://calendar.app.google/sk1G5pjX28zVUg4x9",
-                "_blank"
-              );
-            }
-      }
+      onClick={onClick}
     >
-      <span className="group-hover:opacity-0 transition-opacity duration-300">
-        {children}
-      </span>
-
-      <Video
-        className={cn(
-          "absolute inset-0 m-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white",
-          small ? "w-6 h-6" : "w-8 h-8"
-        )}
-      />
+      {children}
     </button>
   );
 };
