2f701bc4f45dd6ab96013ab2f51d8ff7ab94afd8
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-25 15:41:14 +0500

    feat: use task component inside home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
+import Task from "../../components/task/Task";
 
 const Home = () => {
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <div className="w-full flex flex-col items-center justify-center">
-      HOME SCREEN
+    <div className="w-full flex flex-col items-center justify-center mt-10">
+     <Task />
     </div>
   );
 };
