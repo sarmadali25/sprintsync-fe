e5c7bffea4568f2e6f116ce9d1497c09ab9ec0a3
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-28 17:23:11 +0500

    feat: wrap app in store porvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import AppRouter from "./routing/AppRouter";
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 };
 
 export default App;
