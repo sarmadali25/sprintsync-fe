ddd91303e2ad571ee902af4ec3863cdb5536cf74
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-29 12:15:01 +0500

    feat: export form select component

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,3 +1,4 @@
+export { default as AuthLayout } from "./AuthLayout";
 export { default as FormInput } from "./FormInput";
 export { default as FormButton } from "./FormButton";
-export { default as AuthLayout } from "./AuthLayout";
+export { default as FormSelect } from "./FormSelect";
